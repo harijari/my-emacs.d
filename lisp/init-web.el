8e067ab068d67cf830944587e1cbe60bf965e302
Jarek Sobiecki <jsobiecki@ratioweb.pl>
2017-04-07 08:18:55 +0200

    Enabled less-css-mode

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -7,6 +7,11 @@
 (require-package 'ob-restclient)
 (require-package 'company-restclient)
 
+
+(use-package less-css-mode
+  :ensure t
+  )
+
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 (add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
